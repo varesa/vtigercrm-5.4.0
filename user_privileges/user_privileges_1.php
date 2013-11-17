2e4c13d227488f27882a236456d6192c0e9c04d9
Esa Varemo <esa@kuivanto.fi>
2013-11-17 16:57:13 +0200

    Removed some user_privileges stuff

--- user_privileges/user_privileges_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'$1$ad$xHIFeQyZh.9HICUB8HyrT/','confirm_password'=>'$1$ad$peilFE10wX.4b0aIm7/.t/','first_name'=>'','last_name'=>'Administrator','roleid'=>'H2','email1'=>'juhani@kuivanto.fi','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','currency_id'=>'1','hour_format'=>'am/pm','end_hour'=>'','start_hour'=>'08:00','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','yahoo_id'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'QrVFFXtdhgHEQFI','imagename'=>'','internal_mailer'=>'1','reminder_interval'=>'5 Minutes','asterisk_extension'=>'','use_asterisk'=>'0','ccurrency_name'=>'','currency_code'=>'EUR','currency_symbol'=>'€','conv_rate'=>'1.000','secondaryemail'=>'','theme'=>'','language'=>'','time_zone'=>'UTC','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','record_id'=>'','record_module'=>'','currency_name'=>'Euro','id'=>'1');
-
-?>
